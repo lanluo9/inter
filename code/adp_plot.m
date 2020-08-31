f15a98548dbf42cb8f3f86a24a88a6f85c3648b8
6535 <ll357@duke.edu>
2020-08-31 17:35:52 -0400

    jic

--- code/adp_plot.m
+++ code/adp_plot.m
@@ -92,10 +92,10 @@ adp(iset).adp_ratio_a0t0 = adp_ratio_a0t0;
 
 end
 
-%% adp by area: violin plot and avg-sem
+%% adp by area
+% violin plot and avg-sem
 
 by_area_id = {[1,4,7], [2,5,8], [3,6]}; narea = length(by_area_id);
-by_indiv_id = {[1,2,3], [4,5,6], [7,8]}; nindiv = length(by_indiv_id);
 
 for iarea = 1 : narea
     area_set_seq = by_area_id{iarea};
@@ -103,10 +103,10 @@ for iarea = 1 : narea
     
     for igap = 1 : ngap
     for iset = 1 : length(area_set_seq)
-        temp = adp(area_set_seq(iset)).adp_ratio(:,:,igap); 
-        temp = mean(temp,2);
-        temp = temp(:);
-        adp_ratio_now{igap} = [adp_ratio_now{igap}; temp];
+        tt = adp(area_set_seq(iset)).adp_ratio(:,:,igap); 
+        tt = mean(tt,2);
+        tt = tt(:);
+        adp_ratio_now{igap} = [adp_ratio_now{igap}; tt];
     end
     end
     adp_area(iarea).adp_ratio = adp_ratio_now;
@@ -128,26 +128,24 @@ for igap = 1:ngap
     values(outlier) = NaN;
 
     names = [adp_area(1).name; adp_area(2).name; adp_area(3).name];
-    vs = violinplot(values, names, 'ShowMean', true, 'GroupOrder', {'V1','LM','LI'});
+    vs = violinplot(values, names, 'GroupOrder', {'V1','LM','LI'});
     ylabel(['adaptation index: ', isi_str{igap}]);
     xlim([0.5, 3.5])
     line([0.5, 3.5], [0, 0], 'Color', [0.7,0.7,0.7], 'LineWidth',1, 'LineStyle','--');
 end
-% saveas(gcf, ['adp idx across area w outlier'], 'jpg'); close
+% saveas(gcf, ['adp idx across mouse w outlier'], 'jpg'); 
 % ylim(hAx,[-4, 4])
-% saveas(gcf, ['adp idx across area no outlier'], 'jpg'); close
-ylim(hAx,[-1, 1])
-% saveas(gcf, ['adp idx across area zoom in'], 'jpg'); close 
-
-%%
+% saveas(gcf, ['adp idx across mouse no outlier'], 'jpg'); 
+% ylim(hAx,[-1, 1])
+% saveas(gcf, ['adp idx across mouse zoom in'], 'jpg'); close 
 
 for igap = 1:ngap
     for iarea = 1:narea
-        temp = adp_area(iarea).adp_ratio{1, igap}; 
-        outlier = find(temp > mean(temp(:) + std(temp(:))) | temp < mean(temp(:) - std(temp(:))));
-        temp(outlier) = NaN;
-        adp_area_avg(iarea, igap) = nanmean(temp);
-        adp_area_ste(iarea, igap) = nanstd(temp) ./ length(temp(~isnan(temp)));
+        tt = adp_area(iarea).adp_ratio{1, igap}; 
+        outlier = find(tt > mean(tt(:) + std(tt(:))) | tt < mean(tt(:) - std(tt(:))));
+        tt(outlier) = NaN;
+        adp_area_avg(iarea, igap) = nanmean(tt);
+        adp_area_ste(iarea, igap) = nanstd(tt) ./ length(tt(~isnan(tt)));
     end
 end
 
@@ -163,6 +161,78 @@ ylabel(['adaptation index']);
 legend([h{1,1},h{2,1}], 'isi 750', 'isi 250'); legend boxoff
 xlim([0.5, 3.5])
 ylim([-0.6, 0.1])
-saveas(gcf, ['adp idx across area avg sem'], 'jpg'); close 
+% saveas(gcf, ['adp idx across area avg sem'], 'jpg'); close 
 
+%% adp by mouse
 
+by_mouse_id = {[1,2,3], [4,5,6], [7,8]}; nmouse = length(by_mouse_id);
+adp_mouse = struct;
+
+for imouse = 1 : nmouse
+    mouse_set_seq = by_mouse_id{imouse};
+    adp_ratio_now = cell(1,2);
+    
+    for igap = 1 : ngap
+    for iset = 1 : length(mouse_set_seq)
+        tt = adp(mouse_set_seq(iset)).adp_ratio(:,:,igap); 
+        tt = mean(tt,2);
+        tt = tt(:);
+        adp_ratio_now{igap} = [adp_ratio_now{igap}; tt];
+    end
+    end
+    adp_mouse(imouse).adp_ratio = adp_ratio_now;
+    
+    mouse_name = num2str(dataset_list.mouse(mouse_set_seq(iset))); mouse_name = convertCharsToStrings(mouse_name);
+    adp_mouse(imouse).name = repmat(mouse_name, length(adp_ratio_now{igap}), 1);
+end
+
+cd(result_prefix)
+isi_str = {'isi 750', 'isi 250'}
+
+figure('units','normalized','outerposition',[0 0 1 1]);
+for igap = 1:ngap
+    hAx(igap) = subplot(1,2,igap);
+    values = [adp_mouse(1).adp_ratio{1, igap}; adp_mouse(2).adp_ratio{1, igap};...
+        adp_mouse(3).adp_ratio{1, igap}];
+    outlier = find(values > mean(values(:) + std(values(:))) ...
+        | values < mean(values(:) - std(values(:))));
+    values(outlier) = NaN;
+
+    names = [adp_mouse(1).name; adp_mouse(2).name; adp_mouse(3).name];
+    
+    vs = violinplot(values, names, 'GroupOrder', {'1322','1323','1324'});
+    ylabel(['adaptation index: ', isi_str{igap}]);
+    xlim([0.5, 3.5])
+    line([0.5, 3.5], [0, 0], 'Color', [0.7,0.7,0.7], 'LineWidth',1, 'LineStyle','--');
+end
+% saveas(gcf, ['adp idx across mouse w outlier'], 'jpg'); 
+% ylim(hAx,[-4, 4])
+% saveas(gcf, ['adp idx across mouse no outlier'], 'jpg'); 
+% ylim(hAx,[-1, 1])
+% saveas(gcf, ['adp idx across mouse zoom in'], 'jpg'); close 
+
+for igap = 1:ngap
+    for imouse = 1:nmouse
+        tt = adp_mouse(imouse).adp_ratio{1, igap}; 
+        outlier = find(tt > mean(tt(:) + std(tt(:))) | tt < mean(tt(:) - std(tt(:))));
+        tt(outlier) = NaN;
+        adp_mouse_avg(imouse, igap) = nanmean(tt);
+        adp_mouse_ste(imouse, igap) = nanstd(tt) ./ length(tt(~isnan(tt)));
+    end
+end
+
+% color_list = {[24,95,173], [17,174,207], [176,111,175]}
+color_list = {[0,0,1], [1,0,0]};
+for igap = 1:ngap
+    h{igap,1} = scatter(1:3, adp_mouse_avg(:, igap), 5, color_list{igap}, 'filled'); hold on
+    errorbar(1:3, adp_mouse_avg(:, igap), adp_mouse_ste(:, igap), 'color', color_list{igap}, 'LineStyle','none')
+end
+line([0.5, 3.5], [0, 0], 'Color', [0.7,0.7,0.7], 'LineWidth',1, 'LineStyle','--');
+xticks([1:3]); xticklabels({'1322','1323','1324'})
+ylabel(['adaptation index']);
+legend([h{1,1},h{2,1}], 'isi 750', 'isi 250'); legend boxoff
+xlim([0.5, 3.5])
+ylim([-0.7, 0.1])
+% saveas(gcf, ['adp idx across mouse avg sem'], 'jpg'); close 
+
+%%
