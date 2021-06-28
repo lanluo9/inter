9cf7b453b300bffedc9a8206a1e8942701da0e8b
6535 <ll357@duke.edu>
2021-06-28 12:55:29 -0400

    jic

--- code/mwork/TwoStim_2P_Frames_Jerry-main/twostim_2p_frames/toGrayscale_LL.m
+++ code/mwork/TwoStim_2P_Frames_Jerry-main/twostim_2p_frames/toGrayscale_LL.m
@@ -1,9 +1,9 @@
 clear;
-for i = 6 : 7 %loop through images that need to be changed
+for i = 1 : 7 %loop through images that need to be changed
     filepath = strcat('coloredImages/', int2str(i), '.jpg'); %creates filepath for location of colored image. will need to create "Images" folder if doesn't exist already
     I = rgb2gray(imread(filepath));
     imshow(I)
-%     imsave;
+    saveas(gcf, ['gray_', num2str(i)], 'jpg');
 end
 
 %to make the grayscale images work with mworks, change the .j2c extension to .jpg
