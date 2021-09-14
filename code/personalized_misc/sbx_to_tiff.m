b7aea13c51d2e0b902ac6a461dd71f7c511eb8e6
Lan Luo <ll357@dhe.duke.edu>
2021-09-13 23:54:53 -0400

    jic

--- code/personalized_misc/sbx_to_tiff.m
+++ code/personalized_misc/sbx_to_tiff.m
@@ -1,21 +1,21 @@
-% open sbxread.m
-% open writetiff.m
+clear all
+clc
+
 
 cd('Z:\All_Staff\home\lan\Data\2P_images\i1329\201209\002\')
-% fname = '002_000_000.sbx';
-% sbx2tif(fname)
-
 imgMatFile = '002_000_000.mat';
 load(imgMatFile);
 nframes = info.config.frames;
 data_temp = sbxread(imgMatFile(1,1:11), 0, nframes);
 
 sz = size(data_temp);
-% sz = [sz(2:4), sz(1)];
-% data = reshape(data_temp, sz);
-data = permute(data_temp, [2,3,4,1]);
+% data = permute(data_temp, [2,3,4,1]);
+data = permute(data_temp, [3,2,4,1]); % flip to make nrow > ncol. for easy visualiz
 size(data)
 
-data_chunk = data(:, :, 1:1200); % tif size comparable w caiman demo data
-% fixme: chunk writetif -> concat tif in python
-writetiff(data_chunk, 'Z:\All_Staff\home\lan\Data\2P_images\i1329\201209\002\002.tif')
+data_chunk = data(:, :, 1:1200); % caiman demo data = 3000 frames
+% % fixme: chunk writetif -> concat tif in python
+writetiff(data_chunk, ...
+    'Z:\All_Staff\home\lan\Data\2P_images\i1329\201209\002\002_vertical.tif')
+
+% imshow(squeeze(data_chunk(:,:,1)))
