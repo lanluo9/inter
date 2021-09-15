57e53e167899b4bbc45847f81b00ceacbd74a02a
Lan Luo <ll357@dhe.duke.edu>
2021-09-15 13:57:29 -0400

    jic

--- code/personalized_misc/sbx_to_tiff.m
+++ code/personalized_misc/sbx_to_tiff.m
@@ -1,7 +1,6 @@
 clear all
 clc
 
-
 cd('Z:\All_Staff\home\lan\Data\2P_images\i1329\201209\002\')
 imgMatFile = '002_000_000.mat';
 load(imgMatFile);
@@ -9,13 +8,8 @@ nframes = info.config.frames;
 data_temp = sbxread(imgMatFile(1,1:11), 0, nframes);
 
 sz = size(data_temp);
-% data = permute(data_temp, [2,3,4,1]);
 data = permute(data_temp, [3,2,4,1]); % flip to make nrow > ncol. for easy visualiz
-size(data)
 
-data_chunk = data(:, :, 1:1200); % caiman demo data = 3000 frames
-% % fixme: chunk writetif -> concat tif in python
-writetiff(data_chunk, ...
-    'Z:\All_Staff\home\lan\Data\2P_images\i1329\201209\002\002_vertical.tif')
-
-% imshow(squeeze(data_chunk(:,:,1)))
+% to write large tiff, matlab - pref - general - java heap memory (make larger)
+writetiff(data, 'Z:\All_Staff\home\lan\Data\2P_images\i1329\201209\002\002_vertical_full.tif')
+% writetiff for 100K frames takes about 1 hour
