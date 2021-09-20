e588c14761b0cf4593f72a4bc1aa194dc3d149ff
Lan Luo <lan.luo@duke.edu>
2021-09-20 17:40:35 -0400

    multipage tiff & caiman result load

--- code/personalized_misc/sbx_to_tiff.m
+++ code/personalized_misc/sbx_to_tiff.m
@@ -10,6 +10,12 @@ data_temp = sbxread(imgMatFile(1,1:11), 0, nframes);
 sz = size(data_temp);
 data = permute(data_temp, [3,2,4,1]); % flip to make nrow > ncol. for easy visualiz
 
-% to write large tiff, matlab - pref - general - java heap memory (make larger)
-writetiff(data, 'Z:\All_Staff\home\lan\Data\2P_images\i1329\201209\002\002_vertical_full.tif')
-% writetiff for 100K frames takes about 1 hour
+% % to write large tiff, matlab - pref - general - java heap memory (make larger)
+% writetiff(data, ...
+%     'Z:\All_Staff\home\lan\Data\2P_images\i1329\201209\002\002_vertical_full.tif')
+% % writetiff for 100K frames takes about 1 hour
+% % but it outputs single-page tif, cannot feed to caiman
+
+data = squeeze(data);
+saveastiff(data, ...
+    'Z:\All_Staff\home\lan\Data\2P_images\i1329\201209\002\002_multipage.tif');
