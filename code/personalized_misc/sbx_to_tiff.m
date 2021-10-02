7646046c6992b6d58f69fffe3da6a4a91607a11f
Lan Luo <lan.luo@duke.edu>
2021-10-02 15:34:23 -0400

    jic

--- code/personalized_misc/sbx_to_tiff.m
+++ code/personalized_misc/sbx_to_tiff.m
@@ -9,6 +9,8 @@ load(imgMatFile);
 nframes = info.config.frames;
 nframes
 data_temp = sbxread(imgMatFile(1,1:11), 0, nframes);
+% data_temp = sbxread(imgMatFile(1,1:11), 0, 0);
+size(data_temp)
 disp('read sbx done')
 
 sz = size(data_temp);
@@ -21,14 +23,20 @@ disp('permutation done')
 % % writetiff for 100K frames takes about 1 hour
 % % but it outputs single-page tif, cannot feed to caiman
 
+% elisabeth:
+% data = squeeze(sbxread(fname, 0, 5000));
+% saveastiff(data, ‘fname.tif’);
+
 data = squeeze(data);
-data_chop = data(:,:,1:70000);
+data_chop = data(:,:,1:17000);
+% data_chop = data(:,:,1:70000);
 toc
 
 tic
 disp('start saving tiff')
 saveastiff(data_chop, ...
-    'Z:\All_Staff\home\lan\Data\2P_images\i1329\201209\002\002_multipage_70k.tif');
+    'Z:\All_Staff\home\lan\Data\2P_images\i1329\201209\002\002_multipage_17k.tif');
 % takes 20h to convert 100K frame sbx
 % takes <10h to convert 70K frame sbx? with a tifflib error in the middle ("Unable to write the current directory.")
+% takes 2.5h to convert 35K frame sbx
 toc
