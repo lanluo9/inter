a3b2a3ed7d2507860aabcbef62ec3793c31de78e
Lan Luo hubel <lan.luo@duke.edu>
2022-02-23 18:24:29 -0500

    jic

--- code/py_playground/test.py
+++ code/py_playground/test.py
@@ -1,1 +1,15 @@
-print("hello")
+"""
+_summary_
+"""
+
+def plus(val1, val2):
+    """
+    add two values
+    arguments:
+        val1: first value
+        val2: second value
+    returns:
+        sum of val1 and val2
+    """
+    return val1 + val2
+    
