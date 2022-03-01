a561e054f5b93591922309cae11433d1b0ca3635
Lan Luo <lan.luo@duke.edu>
2022-03-01 16:55:41 -0500

    export conda env list

--- code/personalized_misc/temp.m
+++ code/personalized_misc/temp.m
@@ -1,20 +1,8 @@
-% % Sample Data
-% C = repmat({num2cell(1:11,1)},100,1);
-% % Unpacked
-% C2 = vertcat(C{:})
-% 
-% t2 = vertcat(dfof_ad_trial{:});
-% histogram(t2);
-% t3 = (t2);
-% mean(t3)
-% median(t3)
+t = squeeze(data_temp);
+t_avg = mean(t(:,:,4000:4050), 3);
+size(t_avg)
 
-% tt = dfof_ad_trial{1,:};
-
-
-median(vertcat(dfof_ad_trial{:})), median(vertcat(dfof_tg_trial{:}))
-% dfof_ad
-median(dfof_ad(:)), median(dfof_tg(:))
-
-i = 15
-median(median( (dfof_tg(:,i) - dfof_ad(:,i)) / (dfof_ad(:,i) + 1e-10) ))
+subplot(211)
+imagesc(t(:,:,4050))
+subplot(212)
+imagesc(t_avg)
