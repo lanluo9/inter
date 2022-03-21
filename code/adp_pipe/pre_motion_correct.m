37a8b20c5aa52385b53fa897000db8856977e81f
Lan Luo hubel <lan.luo@duke.edu>
2022-03-21 16:01:02 -0400

    jic

--- code/adp_pipe/pre_motion_correct.m
+++ code/adp_pipe/pre_motion_correct.m
@@ -16,8 +16,10 @@ dataset_now = dataset_meta(ismember(dataset_meta.paradigm, ...
     'bunnytop high res high lum-contrast'),:);
 % dataset_now = dataset_now(dataset_now.mouse == 1369, :);
 dataset_now
+nset = size(dataset_now); nset = nset(1)
 
-iset = 1
+for iset = 1:nset
+
 mouse = dataset_now.mouse(iset)
 imouse = ['i', num2str(mouse)];
 date = num2str(dataset_now.date(iset))
@@ -60,8 +62,16 @@ for i = 1:nep
     title([num2str(1+((i-1)*10000)) '-' num2str(500+((i-1)*10000))])
 end
 
-%%
-select = 4
+%%% select ref episode to register against
+
+clear input
+prompt = "select which episode: ";
+select = input(prompt,"s");
+select = str2num(select)
+if isempty(select)
+    select = 4;
+end
+
 start_idx = select * 10000 + 1;
 stop_idx = select * 10000 + 500;
 data_avg = mean(data(:,:,start_idx:stop_idx),3);
@@ -78,6 +88,7 @@ else
     save(fullfile(LL_base, 'Analysis\2P', [date '_' imouse], [date '_' imouse '_' run_str], [date '_' imouse '_' run_str '_reg_shifts.mat']), 'out', 'data_avg')
     save(fullfile(LL_base, 'Analysis\2P', [date '_' imouse], [date '_' imouse '_' run_str], [date '_' imouse '_' run_str '_input.mat']), 'input')
 end
+size(data_reg)
 
 %%% test stability
 figure; for i = 1:nep; subplot(n,n2,i); imagesc(mean(data_reg(:,:,1+((i-1)*10000):500+((i-1)*10000)),3)); title([num2str(1+((i-1)*10000)) '-' num2str(500+((i-1)*10000))]); end
@@ -87,4 +98,35 @@ print(fullfile(LL_base, 'Analysis\2P', [date '_' imouse], [date '_' imouse '_' r
 figure; imagesq(mean(data_reg(:,:,1:10000),3)); truesize;
 set(gcf, 'Position', get(0, 'Screensize'));
 print(fullfile(LL_base, 'Analysis\2P', [date '_' imouse], [date '_' imouse '_' run_str], [date '_' imouse '_' run_str '_FOV_avg.pdf']),'-dpdf', '-bestfit')
+close all
 
+%%% write mat var to tiff
+
+data_full = permute(data_reg, [2,1,3]); % flip to make nrow > ncol. for easy visualiz
+disp('permutation done')
+data_size = size(data_full)
+nframes = data_size(3)
+
+cd ..
+disp('start saving tiff')
+datetime('now')
+num = ImgFolder;
+tif_file = [imouse, '_', date, '_', num, '_registered.tif'];
+
+fTIF = Fast_BigTiff_Write(tif_file,1,0);
+tic
+msg = 0;
+N = nframes;
+B = numel(data_full)*2;
+for ct = 1:N
+    fprintf(1,repmat('\b',[1,msg]));
+    msg = fprintf(1,'%.0f/%.0f',ct,N);
+    fTIF.WriteIMG(data_full(:,:,ct));
+end
+fTIF.close;
+fprintf(1,repmat('\b',[1,msg]));
+t=toc
+fprintf(1,'\nWrite %.0f bytes in %.0f mins \n',B*N,t/60);
+fprintf(1,'Write speed: %.0f MB/s \n',(B*N)/(2^20*t));
+
+end
