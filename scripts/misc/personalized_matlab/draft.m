aa79e1e85c56507385a1894ee2533ec6a98088fe
Lan Luo hubel <lan.luo@duke.edu>
2022-04-19 00:17:10 -0400

    pymatbridge fails silently. using mutual wait to stitch matlab pipe & py cellpose

--- scripts/misc/personalized_matlab/draft.m
+++ scripts/misc/personalized_matlab/draft.m
@@ -1,6 +1,13 @@
-%% MATLAB
-function lol = draft(args)
-    arg1 = args.arg1;
-    arg2 = args.arg2;
-    lol = arg1 + arg2;
+% %% MATLAB
+% function lol = draft(args)
+%     arg1 = args.arg1;
+%     arg2 = args.arg2;
+%     lol = arg1 + arg2;
+% end
+
+tic
+i=1
+while i<5
+    i = i+1
+    toc
 end
