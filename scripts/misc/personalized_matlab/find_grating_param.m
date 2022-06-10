aa5ce1d60615d3ceb25a45becd0677144c2a5a3e
Lan Luo <ll357@dhe.duke.edu>
2022-06-10 15:42:59 -0400

    generating gabor grating from scratch

---
+++ scripts/misc/personalized_matlab/find_grating_param.m
@@ -0,0 +1,18 @@
+%% search field name in struct
+
+tt = fieldnames(input);
+index = cellfun(@(x) any(contains(lower(x), 'gabor')),tt); sum(index)
+id = find(index > 0);
+for i = 1 : length(id)
+    fprintf(['input.', tt{id(i)}])
+    fprintf('\n')
+end
+
+% % spatial freq
+% input.tGratingSpatialFreqCPD
+% input.gratingSpatialFreqCPD
+% input.block2GratingSpatialFreqCPD
+% input.photoMaskSpatialFreq
+
+% % phase
+% input.gratingStartingPhaseDeg
