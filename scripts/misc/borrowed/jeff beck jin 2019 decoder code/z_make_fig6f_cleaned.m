2860d2e2fea2f40c6cfbb90390aad2b9b406c335
lanluo9 <lan.luo@duke.edu>
2023-11-02 16:00:11 -0400

    jic

--- scripts/misc/borrowed/jeff beck jin 2019 decoder code/z_make_fig6f_cleaned.m
+++ scripts/misc/borrowed/jeff beck jin 2019 decoder code/z_make_fig6f_cleaned.m
@@ -247,6 +247,13 @@ tmp_750 = squeeze(AUROC{k}(:, 2, :));
 row_id = tmp_750(:, 1) > 0;
 tmp_750 = tmp_750(row_id, :);
 
+% figure
+% hold on
+% errorbar(xa, nanmean(tmp_750), ...
+%             nanstd(tmp_750) / norm_ndata, 'b')
+% errorbar(xa, nanmean(tmp_250), ...
+%             nanstd(tmp_250) / norm_ndata, 'r')
+
 figure
 errorbar(xa, nanmedian(tmp_250), ...
             nanstd(tmp_250) / norm_ndata, 'b')
@@ -261,6 +268,6 @@ axis([-5, 95, 0.4, 1])
 legend('250', '750', 'Location','southeast')
 
 cd('C:\Users\ll357\Documents\inter\results\decoder_grat8\pop vec decoder jin2019 jeff')
-save pop_vec_decoder_jeff_res_LI.mat AUROC norm_ndata
+% save pop_vec_decoder_jeff_res_LI.mat AUROC norm_ndata
 
 %%
