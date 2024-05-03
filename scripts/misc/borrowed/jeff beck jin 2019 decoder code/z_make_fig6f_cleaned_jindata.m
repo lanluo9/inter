9be56c7e072626ea64cdc4e710b0f36b9e5beff5
lanluo9 <lan.luo@duke.edu>
2024-05-03 13:47:38 -0400

    replicate pv decoder jin 2019

--- scripts/misc/borrowed/jeff beck jin 2019 decoder code/z_make_fig6f_cleaned_jindata.m
+++ scripts/misc/borrowed/jeff beck jin 2019 decoder code/z_make_fig6f_cleaned_jindata.m
@@ -10,75 +10,86 @@ data_fn = fullfile(ll_fn, 'Data\2P_images');
 mworks_fn = fullfile(fn_base, 'Behavior\Data'); 
 tc_fn = fullfile(ll_fn, 'Analysis\2P');
 
-dir_meta = 'Z:\All_Staff\home\lan\Data\2P_images\mat_inter/adp_dataset_master.xlsx';
-dataset_meta = readtable(dir_meta);
-
-stim_type = 'grating' % grat_8ori_3isi
-dataset_table = dataset_meta(strcmp(dataset_meta.paradigm, stim_type), :);
-
-dataset_table = dataset_table(strcmp(dataset_table.gcamp, '6s'), :);
-seg_bool = dataset_table.manual_seg | dataset_table.cellpose_seg; % exclude not-segmented data
-dataset_table = dataset_table(seg_bool, :);
-
-% % % append a date for multisess 8ori 2isi
-% dataset_table_extend = dataset_meta(dataset_meta.date == 240229, :);
-% dataset_table_extend = dataset_table_extend(1, :); % take first sess as meta
-% dataset_table_extend.num{1} = ''; % accommodate area_mouse_date_sess to multisess (no sess appended)
-% dataset_table = [dataset_table; dataset_table_extend];
-
-select_area = 'V1';
+% dir_meta = 'Z:\All_Staff\home\lan\Data\2P_images\mat_inter/adp_dataset_master.xlsx';
+% dataset_meta = readtable(dir_meta);
+% 
+% stim_type = 'grating' % grat_8ori_3isi
+% dataset_table = dataset_meta(strcmp(dataset_meta.paradigm, stim_type), :);
+% 
+% dataset_table = dataset_table(strcmp(dataset_table.gcamp, '6s'), :);
+% seg_bool = dataset_table.manual_seg | dataset_table.cellpose_seg; % exclude not-segmented data
+% dataset_table = dataset_table(seg_bool, :);
+% 
+% % % % append a date for multisess 8ori 2isi
+% % dataset_table_extend = dataset_meta(dataset_meta.date == 240229, :);
+% % dataset_table_extend = dataset_table_extend(1, :); % take first sess as meta
+% % dataset_table_extend.num{1} = ''; % accommodate area_mouse_date_sess to multisess (no sess appended)
+% % dataset_table = [dataset_table; dataset_table_extend];
+% 
+% % select_area = 'V1';
 % select_area = 'LM';
-% select_area = 'LI';
-area_bool = logical(strcmp(dataset_table.area, select_area));
-dataset_table = dataset_table(area_bool, :);
+% % select_area = 'LI';
+% area_bool = logical(strcmp(dataset_table.area, select_area));
+% dataset_table = dataset_table(area_bool, :);
+% 
+% %%
+% 
+% sum(strcmp(dataset_table.area, 'V1'))
+% sum(strcmp(dataset_table.area, 'LM'))
+% sum(strcmp(dataset_table.area, 'LI'))
+% 
+% nset = size(dataset_table, 1);
+% filename = cell(1, nset);
+% for iset = 1:nset
+%     dataset_now = dataset_table(iset,:);
+%     arg_mouse = dataset_now.mouse;
+%     arg_date = num2str(dataset_now.date);
+%     arg_ImgFolder = dataset_now.num{1};
+%     area = dataset_now.area{1};
+% 
+%     imouse = ['i', num2str(arg_mouse)];
+%     if length(arg_ImgFolder) == 0
+%         area_mouse_date_sess = [area '_' imouse '_' arg_date];
+%     else
+%         area_mouse_date_sess = [area '_' imouse '_' arg_date '_' arg_ImgFolder];
+%     end
+%     mapped_path = 'Z:\All_Staff\home\lan\Data\2P_images';
+%     try
+%         segment_suffix = '_cellpose';
+%         result_folder = [mapped_path, '\mat_inter\', area_mouse_date_sess, segment_suffix];
+%         cd(result_folder)
+%     catch
+%         segment_suffix = '';
+%         result_folder = [mapped_path, '\mat_inter\', area_mouse_date_sess, segment_suffix];
+%         cd(result_folder)
+%     end
+%     % jeff_file = fullfile(result_folder, 'pop_vec_decoder_jeff_control_ncell_vecnorm_visp.mat');
+% 
+%     % pop_vec_decoder_jeff_wellmax_control_ncell_visp
+%     % pop_vec_decoder_jeff_wellmax_control_ncell
+%     % pop_vec_decoder_jeff_10wellmax_vecnorm
+%     jeff_file = fullfile(result_folder, 'pop_vec_decoder_jeff_10wellmax_notnorm.mat');
+%     % if strcmp(select_area, 'LM')
+%     %     jeff_file = fullfile(result_folder, 'pop_vec_decoder_jeff_visp_6k_neighbor_v2.mat');
+%     % end
+%     filename{1, iset} = jeff_file;
+% end
+
+% % Data Preparation
+% % List of file names
+filename = {'170323_i689_runs-002-003', '170323_i696_runs-002-003', '170324_i674_runs-002-003', ...
+            '170327_i684_runs-002-003', '170503_i711_runs-002-003', '170503_i712_runs-002-003', ...
+            '170510_i574_runs-002-003', '170808_i720_runs-002-003', '170810_i738_runs-002-003', ...
+            '170811_i739_runs-002-003', '170816_i745_runs-002-003', '170826_i746_runs-002-003'};
+        
+% _newFits
+for n = 1:length(filename)
+    filename{n} = ['Z:\All_Staff\home\lan\Analysis\optimal decoder from jeff beck dropbox\Lindsay\', ...
+        filename{n}, '_newFits.mat'];
+end
 
 %%
 
-sum(strcmp(dataset_table.area, 'V1'))
-sum(strcmp(dataset_table.area, 'LM'))
-sum(strcmp(dataset_table.area, 'LI'))
-
-nset = size(dataset_table, 1);
-filename = cell(1, nset);
-for iset = 1:nset
-    dataset_now = dataset_table(iset,:);
-    arg_mouse = dataset_now.mouse;
-    arg_date = num2str(dataset_now.date);
-    arg_ImgFolder = dataset_now.num{1};
-    area = dataset_now.area{1};
-    
-    imouse = ['i', num2str(arg_mouse)];
-    if length(arg_ImgFolder) == 0
-        area_mouse_date_sess = [area '_' imouse '_' arg_date];
-    else
-        area_mouse_date_sess = [area '_' imouse '_' arg_date '_' arg_ImgFolder];
-    end
-    mapped_path = 'Z:\All_Staff\home\lan\Data\2P_images';
-    try
-        segment_suffix = '_cellpose';
-        result_folder = [mapped_path, '\mat_inter\', area_mouse_date_sess, segment_suffix];
-        cd(result_folder)
-    catch
-        segment_suffix = '';
-        result_folder = [mapped_path, '\mat_inter\', area_mouse_date_sess, segment_suffix];
-        cd(result_folder)
-    end
-    % jeff_file = fullfile(result_folder, 'pop_vec_decoder_jeff_control_ncell_vecnorm_visp.mat');
-    
-    % pop_vec_decoder_jeff_wellmax_control_ncell_visp
-    % pop_vec_decoder_jeff_wellmax_control_ncell
-    % pop_vec_decoder_jeff_10wellmax_vecnorm
-    jeff_file = fullfile(result_folder, 'pop_vec_decoder_jeff_10wellmax_notnorm.mat');
-    filename{1, iset} = jeff_file;
-end
-
-%% Data Preparation
-% List of file names
-% filename = {'170323_i689_runs-002-003', '170323_i696_runs-002-003', '170324_i674_runs-002-003', ...
-%             '170327_i684_runs-002-003', '170503_i711_runs-002-003', '170503_i712_runs-002-003', ...
-%             '170510_i574_runs-002-003', '170808_i720_runs-002-003', '170810_i738_runs-002-003', ...
-%             '170811_i739_runs-002-003', '170816_i745_runs-002-003', '170826_i746_runs-002-003'};
-        
 PCmax = 15;
 train = 3;
 
@@ -94,8 +105,8 @@ exclude_sess = [];
 %     exclude_sess = [18]; % too many well fit cells, unlikely to be LM
 % end
 
-ncell_good_thresh = 5; % exclude sessions with only 0-n well fit cells
-% theta90_wellfit_thresh = 22.5;
+ncell_good_thresh = 0; % exclude sessions with only 0-n well fit cells
+theta90_wellfit_thresh = 22.5;
 % if strcmp(select_area, 'LI')
 %     theta90_wellfit_thresh = 45; % relax well fit criteria for LI
 % end
@@ -106,8 +117,8 @@ for n = 1:length(filename)
     
     [~, loc] = max(ori_fit);
     prefs{n} = loc / 180 * 2 * pi;
-    % idxn{n} = find(theta_90 < theta90_wellfit_thresh);
-    idxn{n} = find(well_max > 0);
+    idxn{n} = find(theta_90 < theta90_wellfit_thresh);
+    % idxn{n} = find(well_max > 0);
     
     disp(['Dataset ', num2str(n), ' has ', num2str(length(idxn{n})), ' good units']);
     if (length(idxn{n}) <= ncell_good_thresh) % || (length(idxn{n}) > 20)
@@ -118,7 +129,8 @@ end
 sess_kept = logical(sess_kept);
 filename = filename(1, sess_kept);
 disp(['kept ', num2str(sum(sess_kept)), ' out of ', num2str(length(sess_kept)), ...
-    ' in ', select_area])
+    % ' in ', select_area...
+    ])
 
 %%
 
@@ -128,8 +140,8 @@ for n = 1 : length(filename)
     
     [~, loc] = max(ori_fit);
     prefs{n} = loc / 180 * 2 * pi;
-    % idxn{n} = find(theta_90 < theta90_wellfit_thresh);
-    idxn{n} = find(well_max > 0);
+    idxn{n} = find(theta_90 < theta90_wellfit_thresh);
+    % idxn{n} = find(well_max > 0);
     
     disp(['Dataset ', num2str(n), ... 
         ' out of ', num2str(length(filename)), ...
@@ -195,16 +207,17 @@ for n = 1 : length(filename)
     % Remove bias from estimators
     for j = 1:max(train, 2)
         DVAll.dataset = [DVAll.dataset; n * ones(size(DVAlltemp{j}.opt))];
-        % DVAll.dataset = [DVAll.dataset; n * ones(size(DVtemp{j}.opt))];
         DVAll.Y = [DVAll.Y; data{j}.Y];
         DVAll.cond = [DVAll.cond; j * ones(size(data{j}.Y));];
-        DVAll.PV = [DVAll.PV; DVAlltemp{j}.PV];
-        % DVAll.PV = [DVAll.PV; DVtemp{j}.PVemp];
+        % DVAll.PV = [DVAll.PV; DVAlltemp{j}.PV];
+        DVAll.PV = [DVAll.PV; DVtemp{j}.PVemp];
     end
 
     size(DVAll.Y)
     size(DVAll.cond)
     size(DVAll.dataset)
+    size(DVAlltemp{j}.opt)
+    size(DVtemp{j}.opt)
 end
 
 %% auroc
@@ -247,61 +260,61 @@ for dataset = usedatasets
     end
 end
 
-%%
-% % decode each ori against its left neighbor (sorted by ori_dist from adapter)
-% % when ori=0, decode against itself
-
-decoder_mode = 1; % decode neighboring ori
-NDC = 500;
-dv = [0:NDC] / NDC;
-usedatasets = [1 : max(DVAll.dataset)];
-norm_ndata = max(DVAll.dataset);
-
-clear AUROC
-kk=0;
-for dataset = usedatasets
-    kk = kk + 1;
-    for j = 1:2
-        for pair = 1:7 % despite ori_dist having 5 cases, we need to separate neighboring pairs (2-1 vs 6-7)
-            switch pair
-                case 1
-                    ori_cd = 8;
-                    ori_fp = 8;
-                case 2
-                    ori_cd = [1, 7];
-                    ori_fp = 8;
-                case 3
-                    ori_cd = 2; % NOTE: fold up case 3 & 4
-                    ori_fp = 1;
-                case 4
-                    ori_cd = 6;
-                    ori_fp = 7;
-                case 5
-                    ori_cd = 3; % NOTE: fold up case 5 & 6
-                    ori_fp = 2;
-                case 6
-                    ori_cd = 5;
-                    ori_fp = 6;
-                case 7
-                    ori_cd = 4;
-                    ori_fp = [3, 5];
-            end
-            idxfp = (logical(sum(DVAll.Y == ori_fp, 2)) ...
-                & DVAll.cond == j ... % NOTE: in neighbor task, compare within isi condition, instead of always comparing to isi=250 ori=0
-                & logical(sum(DVAll.dataset == dataset, 2)));
-
-            idxcd = (logical(sum(DVAll.Y == ori_cd, 2)) ...
-                & DVAll.cond == j ...
-                & logical(sum(DVAll.dataset == dataset, 2)));
-
-            DVtemp = abs(getfield(DVAll, 'PV'));
-            CD = mean(DVtemp(idxcd) > dv);
-            FP = mean(DVtemp(idxfp) >= dv);
-
-            AUROC{k}(kk, j, pair) = -trapz(FP, CD);
-        end
-    end
-end
+% %%
+% % % decode each ori against its left neighbor (sorted by ori_dist from adapter)
+% % % when ori=0, decode against itself
+% 
+% decoder_mode = 1; % decode neighboring ori
+% NDC = 500;
+% dv = [0:NDC] / NDC;
+% usedatasets = [1 : max(DVAll.dataset)];
+% norm_ndata = max(DVAll.dataset);
+% 
+% clear AUROC
+% kk=0;
+% for dataset = usedatasets
+%     kk = kk + 1;
+%     for j = 1:2
+%         for pair = 1:7 % despite ori_dist having 5 cases, we need to separate neighboring pairs (2-1 vs 6-7)
+%             switch pair
+%                 case 1
+%                     ori_cd = 8;
+%                     ori_fp = 8;
+%                 case 2
+%                     ori_cd = [1, 7];
+%                     ori_fp = 8;
+%                 case 3
+%                     ori_cd = 2; % NOTE: fold up case 3 & 4
+%                     ori_fp = 1;
+%                 case 4
+%                     ori_cd = 6;
+%                     ori_fp = 7;
+%                 case 5
+%                     ori_cd = 3; % NOTE: fold up case 5 & 6
+%                     ori_fp = 2;
+%                 case 6
+%                     ori_cd = 5;
+%                     ori_fp = 6;
+%                 case 7
+%                     ori_cd = 4;
+%                     ori_fp = [3, 5];
+%             end
+%             idxfp = (logical(sum(DVAll.Y == ori_fp, 2)) ...
+%                 & DVAll.cond == j ... % NOTE: in neighbor task, compare within isi condition, instead of always comparing to isi=250 ori=0
+%                 & logical(sum(DVAll.dataset == dataset, 2)));
+% 
+%             idxcd = (logical(sum(DVAll.Y == ori_cd, 2)) ...
+%                 & DVAll.cond == j ...
+%                 & logical(sum(DVAll.dataset == dataset, 2)));
+% 
+%             DVtemp = abs(getfield(DVAll, 'PV'));
+%             CD = mean(DVtemp(idxcd) > dv);
+%             FP = mean(DVtemp(idxfp) >= dv);
+% 
+%             AUROC{k}(kk, j, pair) = -trapz(FP, CD);
+%         end
+%     end
+% end
 
 %% stats
 
@@ -341,9 +354,9 @@ end
 % tmp_250 = tmp.tmp_250;
 % tmp_750 = tmp.tmp_750;
 % 
-% thresh = 0.45
-% tmp_250(tmp_250 < thresh) = 1 - tmp_250(tmp_250 < thresh);
-% tmp_750(tmp_750 < thresh) = 1 - tmp_750(tmp_750 < thresh);
+thresh = 0.45
+tmp_250(tmp_250 < thresh) = 1 - tmp_250(tmp_250 < thresh);
+tmp_750(tmp_750 < thresh) = 1 - tmp_750(tmp_750 < thresh);
 
 figure;
 hold on
@@ -366,6 +379,7 @@ axis([-5, 95, 0.3, 1])
 legend('250', '750', 'Location','southeast')
 
 cd('C:\Users\ll357\Documents\inter\results\decoder_grat8\pop vec decoder jin2019 jeff')
-% save pop_vec_decoder_0vsother_V1_dvall_PV_10wellmax_notnorm.mat tmp_250 tmp_750 AUROC
+% save pop_vec_decoder_jin2019_dvall_pv.mat tmp_250 tmp_750 AUROC
+% save pop_vec_decoder_jin2019_dv_pvemp.mat tmp_250 tmp_750 AUROC
 
 %%
